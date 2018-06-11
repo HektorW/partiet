43a5ab4d6d7d18757fc79863d9d69b6da47a91e5
Hektor Wallin <hektorw@gmail.com>
2018-06-11 22:29:56 +0200

    Update header style

--- client/src/components/MainLayout/MainLayout.js
+++ client/src/components/MainLayout/MainLayout.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import logoSrc from '../../res/logo.jpg'
 import './main-layout.scss'
 
 const MainLayout = ({ children }) => (
   <div className="main-layout">
     <header className="main-layout__header">
+      <Link to="/">
+        <img className="main-layout__header-logo" src={logoSrc} />
+      </Link>
+
       <h1 className="main-layout__header-title">Partiet</h1>
     </header>
     <main className="main-layout__content">{children}</main>
